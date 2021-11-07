0ca413f71104b964cd5004a8a7290a62c80bacc4
jaeyoungha <jaeyoungha@lguplus.co.kr>
2021-11-07 23:28:57 +0900

    test commit add logs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("HJY");
+        System.out.println("HJY >> Config server start!!");
         SpringApplication.run(DemoApplication.class, args);
     }
 
